959457c4626d5d5ac4bc2d22db75940035b33bdc
Bigfoot71 <victor.lejuez@protonmail.ch>
2025-02-16 00:41:40 +0100

    fog example

---
+++ examples/fog.c
@@ -0,0 +1,63 @@
+#include "./common.h"
+#include <r3d.h>
+
+
+/* === Resources === */
+
+static Model		city = { 0 };
+static R3D_Skybox	skybox = { 0 };
+static Camera3D		camera = { 0 };
+
+
+/* === Examples === */
+
+const char* Init(void)
+{
+    R3D_Init(GetScreenWidth(), GetScreenHeight());
+   // SetTargetFPS(60);
+
+    city = RES_LoadModel("sponza.glb");
+
+    for (int i = 0; i < city.materialCount; i++) {
+        city.materials[i].maps[MATERIAL_MAP_ALBEDO].color = WHITE;
+        city.materials[i].maps[MATERIAL_MAP_OCCLUSION].value = 1.0f;
+        city.materials[i].maps[MATERIAL_MAP_ROUGHNESS].value = 1.0f;
+        city.materials[i].maps[MATERIAL_MAP_METALNESS].value = 1.0f;
+    }
+
+    R3D_SetFogMode(R3D_FOG_EXP);
+
+    R3D_Light light = R3D_CreateLight();
+    R3D_SetLightDirection(light, (Vector3) { 0, -1, 0 });
+    R3D_SetLightActive(light, true);
+
+    camera = (Camera3D){
+        .position = (Vector3) { 0, 0, 0 },
+        .target = (Vector3) { 0, 0, -1 },
+        .up = (Vector3) { 0, 1, 0 },
+        .fovy = 60,
+    };
+
+    return "[r3d] - sponza example";
+}
+
+void Update(float delta)
+{
+    UpdateCamera(&camera, CAMERA_FREE);
+}
+
+void Draw(void)
+{
+    R3D_Begin(camera);
+        R3D_DrawModel(city, (Vector3) { 0 }, 1.0f);
+    R3D_End();
+
+    DrawFPS(10, 10);
+}
+
+void Close(void)
+{
+    UnloadModel(city);
+    R3D_UnloadSkybox(skybox);
+    R3D_Close();
+}
