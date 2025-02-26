8ecedae7aae1a3f16bbd5be8148ca68692137f43
Bigfoot71 <victor.lejuez@protonmail.ch>
2025-02-26 07:49:07 +0100

    fix color correction

--- embedded/shaders/screen/adjustment.fs.glsl
+++ embedded/shaders/screen/adjustment.fs.glsl
@@ -34,20 +34,31 @@ uniform float uSaturation;
 
 out vec4 FragColor;
 
-// === Main program === //
+/* === Helper functions === */
+
+vec3 LinearToSRGB(vec3 color)
+{
+	// color = clamp(color, vec3(0.0), vec3(1.0));
+	// const vec3 a = vec3(0.055f);
+	// return mix((vec3(1.0f) + a) * pow(color.rgb, vec3(1.0f / 2.4f)) - a, 12.92f * color.rgb, lessThan(color.rgb, vec3(0.0031308f)));
+	// Approximation from http://chilliant.blogspot.com/2012/08/srgb-approximations-for-hlsl.html
+	return max(vec3(1.055) * pow(color, vec3(0.416666667)) - vec3(0.055), vec3(0.0));
+}
+
+/* === Main program === */
 
 void main()
 {
     // Sampling scene color texture
     vec3 result = texture(uTexColor, vTexCoord).rgb;
 
-    // Apply gamma correction
-    result = pow(result, vec3(1.0/2.2));
-
     // Color adjustment
 	result = mix(vec3(0.0), result, uBrightness);
 	result = mix(vec3(0.5), result, uContrast);
 	result = mix(vec3(dot(vec3(1.0), result) * 0.33333), result, uSaturation);
+
+	// Linear to sRGB conversion
+	result = LinearToSRGB(result);
 
     // Final color output
     FragColor = vec4(result, 1.0);
